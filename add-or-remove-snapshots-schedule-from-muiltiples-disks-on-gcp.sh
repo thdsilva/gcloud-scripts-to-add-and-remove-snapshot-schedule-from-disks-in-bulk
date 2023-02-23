b057723f1adea34aebb4531377d0b5ceac0c5bf3
Thiago Silva <39065653+thdsilva@users.noreply.github.com>
2023-02-23 11:59:04 -0300

    Update add-or-remove-snapshots-schedule-from-muiltiples-disks-on-gcp.sh

--- add-or-remove-snapshots-schedule-from-muiltiples-disks-on-gcp.sh
+++ add-or-remove-snapshots-schedule-from-muiltiples-disks-on-gcp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for disk in  $(gcloud compute disks list --format="value(name)")
 do
-  $(gcloud compute disks add-resource-policies $disk --resource-policies snapshot-vm-prd --zone us-east1-b) 
+  $(gcloud compute disks add-resource-policies $disk --resource-policies [SCHEDULE_NAME] --zone us-east1-b) 
 echo "snapshot-vm-prd added to disk:  $disk"
 done
 
